513f1658b1f469e64a99118aeb2e4eb6e6f5f120
AlfasWeb <108786369+RollyComBr@users.noreply.github.com>
2025-07-05 12:51:37 -0300

    Update flash.h

--- flash.h
+++ flash.h
@@ -8,7 +8,18 @@
 #define PIN_MOSI  23
 #define PIN_MISO  19
 #define PIN_SCK   18
-
+/*
+| W25Q Pino | Nome  | ESP32 Pino sugerido |
+| --------- | ----- | ------------------- |
+| 1         | CS    | GPIO 5              |
+| 2         | DO    | GPIO 19 (MISO)      |
+| 3         | WP    | 3.3V (puxado alto)  |
+| 4         | GND   | GND                 |
+| 5         | DI    | GPIO 23 (MOSI)      |
+| 6         | CLK   | GPIO 18 (SCK)       |
+| 7         | HOLD  | 3.3V (puxado alto)  |
+| 8         | VCC   | 3.3V                |
+*/
 #define FLASH_SECTOR_SIZE    4096
 #define FLASH_PAGE_SIZE      256
 
