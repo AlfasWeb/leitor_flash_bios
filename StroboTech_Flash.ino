10356457ad367d8804d0c6b94ef60f1b90f5e34f
AlfasWeb <108786369+RollyComBr@users.noreply.github.com>
2025-07-05 12:56:47 -0300

    Update StroboTech_Flash.ino

--- StroboTech_Flash.ino
+++ StroboTech_Flash.ino
@@ -1,4 +1,3 @@
-#include "display.h"
 #include "flash.h"
 #include "config.h"
 
@@ -8,6 +7,25 @@ std::vector<Config> imgLogo3;
 std::vector<Config> dadosConfig;
 std::vector<Config> displayConfig;
 
+bool saveData=false;
+
+void exibirImagem(const std::vector<Config>& imagem) {
+  for (const auto& pixel : imagem) {
+    int sep = pixel.chave.indexOf(',');
+    if (sep == -1) continue;
+
+    int x = pixel.chave.substring(0, sep).toInt();
+    int y = pixel.chave.substring(sep + 1).toInt();
+
+    int cor = pixel.valor.toInt();  // Deve ser "1" ou "0"
+
+    display.drawPixel(x, y, cor ? WHITE : BLACK);
+  }
+
+  display.display();  // Atualiza o display com todos os pixels
+}
+
+
 void setup() {
   Serial.begin(115200);
   while (!Serial);
@@ -15,19 +33,36 @@ void setup() {
   iniciarSPIFlash();
   identificarJEDEC();
 
-  carregarArquivo(displayConfig, 0x00680, 0x001AD1);
-  carregarArquivo(dadosConfig, 0x001AD2, 0x001B6B);
+  carregarImagem(imgLogo1, 0x000000, 0x0003FF);  // endereço da imagem 1
+  exibirImagem(imgLogo1, 128, 64, 0, 0);
+  display.clearDisplay();
+  delay(2000);
+  carregarImagem(imgLogo2, 0x00041F, 0x00061E);  // endereço da imagem 1
+  exibirImagem(imgLogo2, 128, 32, 90, 15);
+  display.clearDisplay();
+  delay(2000);
+  carregarImagem(imgLogo3, 0x00063E, 0x0006BD);  // endereço da imagem 1
+  exibirImagem(imgLogo3, 32, 32, 0, 20);
 
-  for (auto& c : dadosConfig) {
+  //carregarArquivo(displayConfig, 0x00680, 0x001AD1);
+  carregarArquivo(dadosConfig, 0x001B92, 0x001BD4);
+  numIdioma = getValor(dadosConfig, "IDIOMA").toInt();
+  idioma = getSiglaIdioma(numIdioma);
+  delay(100);
+  carregarArquivoParcial(displayConfig, 0x006DD, 0x001B60, idioma);
+  display.clearDisplay();
+  delay(2000);
+  for (auto& c : displayConfig) {
     Serial.println(c.chave + "=" + c.valor);
   }
-
+  /*
   setValor(dadosConfig, "IDIOMA", "1");
   setValor(dadosConfig, "TIMEMEASURE", "30");
   setValor(dadosConfig, "TIMECALIB", "10");
   setValor(dadosConfig, "FPM", "3000");
   setValor(dadosConfig, "INDICEARQUIVO", "0");
   setValor(dadosConfig, "NEW", "0");
+  
 
   if(salvarArquivo(dadosConfig, 0x001AD2, 0x001B6B)){
     //Serial.println("Listando dadosConfig:");
@@ -35,8 +70,14 @@ void setup() {
       Serial.println(c.chave + "=" + c.valor);
     }
   }
+  */
 }
 
 void loop() {
+  if(saveData){
+    if(salvarArquivo(dadosConfig, 0x001B92, 0x001BD4)){
+      saveData=false;
+    }
+  }
   // nada aqui
 }
