27652e2f26f43e1267cc6b1fb2ce931ff16d06ed
AlfasWeb <108786369+RollyComBr@users.noreply.github.com>
2025-07-05 12:50:47 -0300

    Update config.h

--- config.h
+++ config.h
@@ -9,9 +9,11 @@ struct Config {
   String valor;
 };
 
-void carregarArquivo(std::vector<Config>& destino, uint32_t addrIni, uint32_t addrFim);
+bool carregarArquivo(std::vector<Config>& destino, uint32_t addrIni, uint32_t addrFim);
 bool salvarArquivo(const std::vector<Config>& lista, uint32_t addrIni, uint32_t addrFim);
 String getValor(const std::vector<Config>& lista, const String& chave, const String& idioma = "");
 void setValor(std::vector<Config>& lista, const String& chave, const String& valor, const String& idioma = "");
+void carregarArquivoParcial(std::vector<Config>& destino, uint32_t addrIni, uint32_t addrFim, const String& idioma = "");
+void carregarImagem(std::vector<Config>& destino, uint32_t addrIni, uint32_t addrFim);
 
 #endif
