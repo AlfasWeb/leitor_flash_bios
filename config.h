e54256713e7e5c1a4be61026735c9e8e5890cfdc
AlfasWeb <108786369+AlfasWeb@users.noreply.github.com>
2025-07-18 15:34:12 -0300

    Update config.h

--- config.h
+++ config.h
@@ -12,8 +12,11 @@ struct Config {
 bool carregarArquivo(std::vector<Config>& destino, uint32_t addrIni, uint32_t addrFim);
 bool salvarArquivo(const std::vector<Config>& lista, uint32_t addrIni, uint32_t addrFim);
 String getValor(const std::vector<Config>& lista, const String& chave, const String& idioma = "");
-void setValor(std::vector<Config>& lista, const String& chave, const String& valor, const String& idioma = "");
+bool setValor(std::vector<Config>& lista, const String& chave, const String& valor, const String& idioma = "");
 void carregarArquivoParcial(std::vector<Config>& destino, uint32_t addrIni, uint32_t addrFim, const String& idioma = "");
 void carregarImagem(std::vector<Config>& destino, uint32_t addrIni, uint32_t addrFim);
-
+// Define o tipo de callback para processar pixels
+typedef void (*PixelCallback)(int x, int y, bool isOn);
+// Declara a função que exibe a imagem
+void exibirImagemDaFlash(uint32_t enderecoInicial, int largura, int altura, int offsetX, int offsetY, PixelCallback callback);
 #endif
